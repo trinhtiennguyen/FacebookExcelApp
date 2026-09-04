6a857ee407faf0f40ffcf296e45fa655e0a109fe
trinhtiennguyen <tunglam062000@gmail.com>
2026-09-04 19:43:47 +0700

    Refactor build.gradle.kts formatting and structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,24 +25,28 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-}
 
-buildFeatures {
-	compose = true
-}
+    buildFeatures {
+        compose = true
+    }
 
-packaging {
-	resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
-
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
     implementation("androidx.activity:activity-compose:1.10.0")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
