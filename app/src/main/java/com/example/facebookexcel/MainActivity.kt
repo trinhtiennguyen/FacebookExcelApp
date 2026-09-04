77d4c037d3112bcc5b20b484ae2ed47c1e3a8ac0
trinhtiennguyen <tunglam062000@gmail.com>
2026-09-04 19:50:15 +0700

    Add OptIn annotation for ExperimentalMaterial3Api

--- app/src/main/java/com/example/facebookexcel/MainActivity.kt
+++ app/src/main/java/com/example/facebookexcel/MainActivity.kt
@@ -46,6 +46,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FacebookExcelScreen(vm: MainViewModel = viewModel()) {
     val items by vm.items.collectAsState()
