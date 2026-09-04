d7a778953eb7a482d3cf9a803512a8627055cdae
trinhtiennguyen <tunglam062000@gmail.com>
2026-09-04 19:34:13 +0700

    Set Java and Kotlin compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,16 @@ android {
         versionName = "1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
     buildFeatures {
         compose = true
     }
