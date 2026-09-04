c0acd376b5f9b5f5a61ffe1ae76d7163cdd89296
trinhtiennguyen <tunglam062000@gmail.com>
2026-09-04 19:37:36 +0700

    Update dependencies in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-}
+
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
